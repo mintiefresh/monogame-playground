f11450b2b8e22b9b5976311fe7baa2ed4cca6ab9
Minh Hoang <minhtuan@ualberta.ca>
2022-12-28 14:35:22 -0700

    Added assets into content pipeline; Changed resolution to 1280 x 720;

--- rpg/src/rpg/Game1.cs
+++ rpg/src/rpg/Game1.cs
@@ -9,6 +9,21 @@ namespace rpg
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // CONSTANTS
+        const int RESOLUTION_WIDTH = 1280;
+        const int RESOLUTION_HEIGHT = 720;
+
+        // VARIABLES
+        Texture2D playerSprite;
+        Texture2D walkDown;
+        Texture2D walkUp;
+        Texture2D walkRight;
+        Texture2D walkLeft;
+
+        Texture2D background;
+        Texture2D ball;
+        Texture2D skull;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,6 +34,10 @@ namespace rpg
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = RESOLUTION_WIDTH;
+            _graphics.PreferredBackBufferHeight = RESOLUTION_HEIGHT;
+            _graphics.ApplyChanges();
+
 
             base.Initialize();
         }
@@ -43,9 +62,12 @@ namespace rpg
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
 
             // TODO: Add your drawing code here
 
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
